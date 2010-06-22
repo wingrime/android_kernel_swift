c4d5e0c98b5b60aa00865b55ff20cec82cb7d09c
João Paulo Rechi Vita <jprvita@profusion.mobi>
2010-06-22 13:56:22 -0300

    Bluetooth: Fix l2cap_sock_connect error return.
    Return a proper error value if socket is already connected.
    
    Signed-off-by: João Paulo Rechi Vita <jprvita@profusion.mobi>
    Acked-by: Gustavo F. Padovan <padovan@profusion.mobi>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit 8b0dc6dc827fb71efd6c9dfb5c4172b7b5c96cee)
    
    Change-Id: Ic82f9396c88babf1789b20926d3b6505bd5c675e
    Signed-off-by: Mat Martineau <mathewm@codeaurora.org>

--- net/bluetooth/l2cap.c
+++ net/bluetooth/l2cap.c
@@ -1167,6 +1167,7 @@ static int l2cap_sock_connect(struct socket *sock, struct sockaddr *addr, int al
 
 	case BT_CONNECTED:
 		/* Already connected */
+		err = -EISCONN;
 		goto done;
 
 	case BT_OPEN:

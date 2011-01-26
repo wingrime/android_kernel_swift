64e56bac58ef4aded58d01e7843b177cee169bb2
Asish Bhattacharya <asishb@codeaurora.org>
2011-01-26 09:04:11 +0530

    ASoC: msm8x60: Correction in Licence text.
    Change-Id: Iab85de54f60779eb5a4995bb03dcf76638af738a
    Signed-off-by: Asish Bhattacharya <asishb@codeaurora.org>

--- sound/soc/msm/msm8x60-pcm.c
+++ sound/soc/msm/msm8x60-pcm.c
@@ -1,21 +1,19 @@
-/* Copyright (c) 2010, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
  *
- * All source code in this file is licensed under the following license except
- * where indicated.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, you can find it at http://www.fsf.org.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  */
-
 
 #include <linux/init.h>
 #include <linux/err.h>

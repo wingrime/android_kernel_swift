231f2e1ed302027e3457eb1a063b46ab33c0ab5b
Stephen Boyd <sboyd@codeaurora.org>
2011-01-26 17:10:38 -0800

    msm: 8960: Add stub 8960 clocks
    Add clock stubs for 8960 as well as a new driver to just always
    return success for any clock operations.
    
    Change-Id: I8d30263d1fd1d46913d8c012de6435af5492f78f
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -103,6 +103,9 @@ extern unsigned msm_num_clocks_8x50;
 extern struct clk_lookup msm_clocks_8x60[];
 extern unsigned msm_num_clocks_8x60;
 
+extern struct clk_lookup msm_clocks_8960[];
+extern unsigned msm_num_clocks_8960;
+
 void __init msm_fb_register_device(char *name, void *data);
 void __init msm_camera_register_device(void *, uint32_t, void *);
 extern struct platform_device msm_device_touchscreen;

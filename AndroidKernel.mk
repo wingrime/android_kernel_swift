413b99ccf534e504a3c555cdce527fa0e81bb0b6
Tianyi Gou <tgou@codeaurora.org>
2011-02-24 12:07:49 -0800

    AndroidKernel.mk: install kernel modules to target filesystem
    Install all in tree kernel modules to /system/lib/modules on the
    target using make modules_install to avoid maintaining a list of
    copy rules for each modules. Modules are moved to /system/lib/modules
    to make the module path fixed for easier maintainance in other files
    using modules. All other non-module files are cleaned.
    
    Change-Id: I3e65949b92b0d6b7e487419d0e6b11bb0945e986
    Signed-off-by: Tianyi Gou <tgou@codeaurora.org>

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -6,6 +6,8 @@ KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
 KERNEL_HEADERS_INSTALL := $(KERNEL_OUT)/usr
+KERNEL_MODULES_INSTALL := system
+KERNEL_MODULES_OUT := $(TARGET_OUT)/lib/modules
 
 ifeq ($(TARGET_USES_UNCOMPRESSED_KERNEL),true)
 $(info Using uncompressed kernel)
@@ -13,6 +15,19 @@ TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/piggy
 else
 TARGET_PREBUILT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)
 endif
+
+define mv-modules
+rm -f $(KERNEL_MODULES_OUT)/*.ko;\
+ko=`find $(KERNEL_MODULES_OUT) -type f -name *.ko`;\
+for i in $$ko; do mv $$i $(KERNEL_MODULES_OUT)/; done
+endef
+
+define clean-module-folder
+mdpath=`find $(KERNEL_MODULES_OUT) -type f -name modules.dep`;\
+if [ "$$mdpath" != "" ];then\
+mpath=`dirname $$mdpath`; rm -rf $$mpath;\
+fi
+endef
 
 file := $(TARGET_OUT)/lib/modules/oprofile.ko
 ALL_PREBUILT += $(file)
@@ -31,6 +46,9 @@ $(KERNEL_OUT)/piggy : $(TARGET_PREBUILT_INT_KERNEL)
 $(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_HEADERS_INSTALL)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi-
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- modules
+	$(MAKE) -C kernel O=../$(KERNEL_OUT) INSTALL_MOD_PATH=../../$(KERNEL_MODULES_INSTALL) ARCH=arm CROSS_COMPILE=arm-eabi- modules_install
+	$(mv-modules)
+	$(clean-module-folder)
 
 $(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT) $(KERNEL_CONFIG)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=arm-eabi- headers_install

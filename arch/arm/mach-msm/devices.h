a666f6e53d2cef9257514989ac588eb0b2ffc308
Kenneth Heitke <kheitke@codeaurora.org>
2011-03-04 12:23:50 -0700

    msm: Add I2C QUP support for MSM8960
    Add the device definitions and platform data to support
    I2C QUP on MSM8960 simulator and RUMI3 targets.
    
    Change-Id: I44c9baf80d170710ba0dab498a6a11e432f923c5
    Signed-off-by: Kenneth Heitke <kheitke@codeaurora.org>

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -34,6 +34,7 @@ extern struct platform_device msm_device_uart_gsbi9;
 
 extern struct platform_device msm8960_device_uart_gsbi2;
 extern struct platform_device msm8960_device_uart_gsbi5;
+extern struct platform_device msm8960_device_qup_i2c_gsbi4;
 
 extern struct platform_device msm_device_sdc1;
 extern struct platform_device msm_device_sdc2;

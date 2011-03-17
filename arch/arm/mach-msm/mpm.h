94abb27243e0ad5e20883d92f5b4c6d9d4f71e34
Ai Li <aili@codeaurora.org>
2011-03-17 13:25:28 -0600

    msm: mpm: add enable_pin function
    The new function allows clients to program the MPM signal pins as
    wakeup sources during idle sleep whereas the existing set_pin_wake
    function does for suspend sleep.
    
    Change-Id: I590d5df2b91862f71ba12e601970e9eb266dc523
    Signed-off-by: Ai Li <aili@codeaurora.org>

--- arch/arm/mach-msm/mpm.h
+++ arch/arm/mach-msm/mpm.h
@@ -60,6 +60,7 @@ extern struct msm_mpm_device_data msm_mpm_dev_data;
 int msm_mpm_enable_irq(unsigned int irq, unsigned int enable);
 int msm_mpm_set_irq_wake(unsigned int irq, unsigned int on);
 int msm_mpm_set_irq_type(unsigned int irq, unsigned int flow_type);
+int msm_mpm_enable_pin(enum msm_mpm_pin pin, unsigned int enable);
 int msm_mpm_set_pin_wake(enum msm_mpm_pin pin, unsigned int on);
 int msm_mpm_set_pin_type(enum msm_mpm_pin pin, unsigned int flow_type);
 bool msm_mpm_irqs_detectable(bool from_idle);
@@ -73,6 +74,8 @@ int msm_mpm_enable_irq(unsigned int irq, unsigned int enable)
 int msm_mpm_set_irq_wake(unsigned int irq, unsigned int on)
 { return -ENODEV; }
 int msm_mpm_set_irq_type(unsigned int irq, unsigned int flow_type)
+{ return -ENODEV; }
+int msm_mpm_enable_pin(enum msm_mpm_pin pin, unsigned int enable)
 { return -ENODEV; }
 int msm_mpm_set_pin_wake(enum msm_mpm_pin pin, unsigned int on)
 { return -ENODEV; }

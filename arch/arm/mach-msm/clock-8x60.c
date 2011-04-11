df7e3fa8e832a761fdcaafe6cf3fd08b56d57e92
Matt Wagantall <mattw@codeaurora.org>
2011-04-11 10:13:53 -0700

    msm: clock-8x60: Refresh PDM and TSSC halt bits at boot
    The halt status bits for PDM and TSSC may be incorrect at boot.
    Toggle these clocks on and off to refresh them.
    
    This resolves the "local_clk_disable_reg: clock 33 status stuck
    at 'on'" message seen at boot for TSSC clock when the auto-off
    code attempts to ensure unused clocks are off at late_init. The
    fix is purely cosmetic: both the PDM and TSSC clocks function
    correctly without it.
    
    Change-Id: I7f5b2b0b3519245409b1943ebd78638ef3a0ee46
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/mach-msm/clock-8x60.c
+++ arch/arm/mach-msm/clock-8x60.c
@@ -2509,6 +2509,13 @@ void __init msm_clk_soc_init(void)
 	local_clk_set_rate(C(USB_HS1_XCVR), 60000000);
 	local_clk_set_rate(C(USB_FS1_SRC), 60000000);
 	local_clk_set_rate(C(USB_FS2_SRC), 60000000);
+
+	/* The halt status bits for PDM and TSSC may be incorrect at boot.
+	 * Toggle these clocks on and off to refresh them. */
+	local_clk_enable(C(PDM));
+	local_clk_disable(C(PDM));
+	local_clk_enable(C(TSSC));
+	local_clk_disable(C(TSSC));
 }
 
 static int msm_clk_soc_late_init(void)

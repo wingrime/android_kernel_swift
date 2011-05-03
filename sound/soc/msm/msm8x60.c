d87ca4616c9ea0104b8f99935e826af97aa838bf
Bharath Ramachandramurthy <bramacha@codeaurora.org>
2011-05-03 16:24:32 -0700

    ASOC: msm8x60: Change the volume gain  for 0db.
    CRs-fixed: 281159
    Signed-off-by: Bharath Ramachandramurthy <bramacha@codeaurora.org>
    (cherry picked from commit 061ab3acb2902a4c8e2732ca04e994e30ddf36d9)
    
    Change-Id: I1f09b9244ba44393465977ba77127afeff9fc240
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- sound/soc/msm/msm8x60.c
+++ sound/soc/msm/msm8x60.c
@@ -51,7 +51,7 @@ EXPORT_SYMBOL(session_route);
 static struct snd_kcontrol_new snd_msm_controls[];
 
 char snddev_name[AUDIO_DEV_CTL_MAX_DEV][44];
-#define MSM_MAX_VOLUME 0x3FFF
+#define MSM_MAX_VOLUME 0x2000
 #define MSM_VOLUME_STEP ((MSM_MAX_VOLUME+17)/100) /* 17 added to avoid
 						      more deviation */
 static int device_index; /* Count of Device controls */

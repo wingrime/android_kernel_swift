6a9be34e34236c30817718a834aaba1cb2ffffe8
Alexsey Shestakov <wingrime@gmail.com>
2012-03-19 17:44:25 +0600

    Remove unstable overclock

--- arch/arm/mach-msm/acpuclock.c
+++ arch/arm/mach-msm/acpuclock.c
@@ -17,8 +17,6 @@
  *
  */
 
-//#define OVERCLOCK_AHB
-
 #include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -65,7 +63,7 @@ struct clock_state {
 	uint32_t			max_speed_delta_khz;
 	uint32_t			vdd_switch_time_us;
 	unsigned long			max_axi_khz;
-       struct clk                       *ebi1_clk;
+	struct clk			*ebi1_clk;
 };
 
 #define PLL_BASE	7
@@ -212,35 +210,6 @@ static struct clkctl_acpu_speed pll0_960_pll1_245_pll2_1200[] = {
 	{ 0, 400000, ACPU_PLL_2, 2, 2, 133333, 2, 5, 122880 },
 	{ 1, 480000, ACPU_PLL_0, 4, 1, 160000, 2, 6, 122880 },
 	{ 1, 600000, ACPU_PLL_2, 2, 1, 200000, 2, 7, 122880 },
-#ifndef OVERCLOCK_AHB
-/* Conservative AHB overclocking */
-	{ 1, 652800, ACPU_PLL_0, 4, 0, 217600, 2, 7, 200000 },
-	{ 1, 672000, ACPU_PLL_0, 4, 0, 224000, 2, 7, 200000 },
-	{ 1, 691200, ACPU_PLL_0, 4, 0, 230400, 2, 7, 200000 },
-	{ 1, 710400, ACPU_PLL_0, 4, 0, 236800, 2, 7, 200000 },
-	{ 1, 729600, ACPU_PLL_0, 4, 0, 243200, 2, 7, 200000 },
-	{ 1, 748800, ACPU_PLL_0, 4, 0, 249600, 2, 7, 200000 },
-	{ 1, 768000, ACPU_PLL_0, 4, 0, 256000, 2, 7, 200000 },
-	{ 1, 787200, ACPU_PLL_0, 4, 0, 262400, 2, 7, 200000 },
-	{ 1, 806400, ACPU_PLL_0, 4, 0, 268800, 2, 7, 200000 },
-	{ 1, 825600, ACPU_PLL_0, 4, 0, 275200, 2, 7, 200000 },
-	{ 1, 844800, ACPU_PLL_0, 4, 0, 281600, 2, 7, 200000 },
-#else
-/* Agressive AHB overclocking */
-//	{ 1, 480000, ACPU_PLL_0, 4, 1, 240000, 1, 6, 200000 },
-//	{ 1, 600000, ACPU_PLL_2, 2, 1, 300000, 1, 7, 200000 },
-	{ 1, 652800, ACPU_PLL_0, 4, 0, 326400, 1, 7, 200000 },
-	{ 1, 672000, ACPU_PLL_0, 4, 0, 336000, 1, 7, 200000 },
-	{ 1, 691200, ACPU_PLL_0, 4, 0, 345600, 1, 7, 200000 },
-	{ 1, 710400, ACPU_PLL_0, 4, 0, 355200, 1, 7, 200000 },
-	{ 1, 729600, ACPU_PLL_0, 4, 0, 364800, 1, 7, 200000 },
-	{ 1, 748800, ACPU_PLL_0, 4, 0, 374400, 1, 7, 200000 },
-	{ 1, 768000, ACPU_PLL_0, 4, 0, 384000, 1, 7, 200000 },
-	{ 1, 787200, ACPU_PLL_0, 4, 0, 393600, 1, 7, 200000 },
-	{ 1, 806400, ACPU_PLL_0, 4, 0, 403200, 1, 7, 200000 },
-//	{ 1, 825600, ACPU_PLL_0, 4, 0, 412800, 1, 7, 200000 },
-//	{ 1, 844800, ACPU_PLL_0, 4, 0, 422400, 1, 7, 200000 },
-#endif
 	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, {0, 0, 0}, {0, 0, 0} }
 };
 
@@ -353,6 +322,12 @@ static void __init cpufreq_table_init(void)
 }
 #endif
 
+unsigned long clk_get_max_axi_khz(void)
+{
+	return drv_state.max_axi_khz;
+}
+EXPORT_SYMBOL(clk_get_max_axi_khz);
+
 static int pc_pll_request(unsigned id, unsigned on)
 {
 	int res = 0;
@@ -455,21 +430,6 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s)
 	/* CLK_SEL_SRC1NO */
 	src_sel = reg_clksel & 1;
 
-	// Perform overclocking if requested
-	if(hunt_s->pll==0 && hunt_s->a11clk_khz>600000) {
-		// Change the speed of PLL0
-		writel(hunt_s->a11clk_khz/19200, PLLn_L_VAL(0));
-		udelay(50);
-	}
-
-#ifdef OVERCLOCK_AHB
-	// Pump the PLL2 up another 19200kHz (overclock stock 600MHz from 595.2MHz to 604.8MHz)
-	if(hunt_s->pll==2 && hunt_s->a11clk_khz==600000) {
-		writel(63, PLLn_L_VAL(2));
-		udelay(50);
-	}
-#endif
-
 	/*
 	 * If the new clock divider is higher than the previous, then
 	 * program the divider before switching the clock
@@ -479,7 +439,6 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s)
 		reg_clksel |= (hunt_s->ahbclk_div << 1);
 		writel(reg_clksel, A11S_CLK_SEL_ADDR);
 	}
-
 
 	/* Program clock source and divider */
 	reg_clkctl = readl(A11S_CLK_CNTL_ADDR);
@@ -491,7 +450,6 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s)
 	/* Program clock source selection */
 	reg_clksel ^= 1;
 	writel(reg_clksel, A11S_CLK_SEL_ADDR);
-
 
 	/*
 	 * If the new clock divider is lower than the previous, then
@@ -644,8 +602,8 @@ int acpuclk_set_rate(int cpu, unsigned long rate, enum setrate_reason reason)
 
 	/* Change the AXI bus frequency if we can. */
 	if (strt_s->axiclk_khz != tgt_s->axiclk_khz) {
-                 res = clk_set_rate(drv_state.ebi1_clk,
-                               tgt_s->axiclk_khz * 1000);
+		res = clk_set_rate(drv_state.ebi1_clk,
+				tgt_s->axiclk_khz * 1000);
 		if (res < 0)
 			pr_warning("Setting AXI min rate failed (%d)\n", res);
 	}
@@ -730,7 +688,7 @@ static void __init acpuclk_init(void)
 		pr_warning("Setting AXI min rate failed (%d)\n", res);
 	res = clk_enable(drv_state.ebi1_clk);
 	if (res < 0)
-	        pr_warning("Enabling AXI clock failed (%d)\n", res);
+		pr_warning("Enabling AXI clock failed (%d)\n", res);
 
 	pr_info("ACPU running at %d KHz\n", speed->a11clk_khz);
 }

47d7b29ac4bf81f09b9e316a1e919505c8523a85
Alexsey Shestakov <wingrime@gmail.com>
2012-03-26 21:53:06 +0600

    Fix kernel opps: Key xxxxxx not in data

--- arch/arm/mach-msm/pm2.c
+++ arch/arm/mach-msm/pm2.c
@@ -311,7 +311,7 @@ static int __init msm_pm_mode_sysfs_add(void)
 			kobj_attrs[k].attr.mode = 0644;
 			kobj_attrs[k].show = msm_pm_mode_attr_show;
 			kobj_attrs[k].store = msm_pm_mode_attr_store;
-
+			sysfs_attr_init(&kobj_attrs[k].attr);
 			attrs[k] = &kobj_attrs[k].attr;
 		}
 		attrs[MSM_PM_MODE_ATTR_NR] = NULL;
